276ff41fd3c0df978e5e507fd3e401f9b5914c13
Gleyson Abreu <gleyson_datu@hotmail.com>
2019-10-14 20:38:59 -0300

    Ajustes no design da add cardapio, e também mostrar e adicionar em tempo real as categorias adicionadas.

--- app/src/main/java/com/gleysonabreu/cardatronicoempresa/fragment/AddCardapioFragment.kt
+++ app/src/main/java/com/gleysonabreu/cardatronicoempresa/fragment/AddCardapioFragment.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,8 +14,13 @@ import android.widget.Toast
 
 import com.gleysonabreu.cardatronicoempresa.R
 import com.gleysonabreu.cardatronicoempresa.activity.CategoryActivity
+import com.gleysonabreu.cardatronicoempresa.helper.SettingsFirebase
 import com.gleysonabreu.cardatronicoempresa.model.Category
 import com.google.android.material.chip.Chip
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.fragment_add_cardapio.*
 import kotlinx.android.synthetic.main.fragment_add_cardapio.view.*
 import kotlinx.android.synthetic.main.fragment_add_cardapio.view.buttonOpenCategory
@@ -24,70 +30,86 @@ import kotlinx.android.synthetic.main.fragment_add_cardapio.view.buttonOpenCateg
  */
 class AddCardapioFragment : Fragment() {
 
+    private lateinit var referenceDatabase: DatabaseReference;
+    private lateinit var viewOfLayout: View;
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        var viewOfLayout = inflater.inflate(R.layout.fragment_add_cardapio, container, false)
+        viewOfLayout = inflater.inflate(R.layout.fragment_add_cardapio, container, false)
 
         //Open Add Category
         viewOfLayout.buttonOpenCategory.setOnClickListener {
-
             var i: Intent = Intent(activity, CategoryActivity::class.java);
             startActivity(i)
-
         }
+            // Settings ChipGroup
+            viewOfLayout.chip_group.isSingleSelection = true;
 
-        viewOfLayout.chip_group.isSingleSelection = true;
-        val chip: Chip = Chip(activity);
-        chip.text = "Kotlin";
-        chip.setChipBackgroundColorResource(R.color.colorSplash);
-        chip.setTextColor(Color.WHITE)
-        chip.chipCornerRadius = 16F;
-        chip.isCheckable = true;
 
-        viewOfLayout.chip_group.addView(chip);
+            //Settings firebase
+            referenceDatabase = SettingsFirebase.getFirebase();
+            var categorys = referenceDatabase.child("category");
 
-        val chip2: Chip = Chip(activity);
-        chip2.text = "Java";
-        chip2.setChipBackgroundColorResource(R.color.colorSplash);
-        chip2.setTextColor(Color.WHITE)
-        chip2.chipCornerRadius = 16F;
-        chip2.isCheckable = true;
-
-        viewOfLayout.buttonTest.setOnClickListener {
-
-            var msg = "Chip selecionado: ";
-            var chipGroup = viewOfLayout.chip_group;
-            var count: Int = chipGroup.childCount;
-            if( count == 0){
-                msg += " nenhum selecionado";
-            }else{
-
-                var i: Int = 0;
-                while (i < count){
-                    var chip: Chip = chipGroup.getChildAt(i) as Chip;
-                    if(chip.isChecked == true){
-                        msg += chip.text.toString();
+            categorys.addValueEventListener(
+                object : ValueEventListener {
+                    override fun onCancelled(p0: DatabaseError) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
                     }
-                    i++;
+
+                    override fun onDataChange(p0: DataSnapshot) {
+                        viewOfLayout.chip_group.removeAllViews();
+
+                        for (cats in p0.children) {
+                            var category = cats.getValue(Category::class.java);
+                            val chip = Chip(activity);
+                            chip.text = category?.nameCategory;
+                            chip.setChipBackgroundColorResource(R.color.colorSplash);
+                            chip.setTextColor(Color.WHITE);
+                            chip.chipCornerRadius = 16F;
+                            chip.isCheckable = true;
+
+                            viewOfLayout.chip_group.addView(chip);
+                        }
+
+                    }
+
+                });
+
+            viewOfLayout.buttonTest.setOnClickListener {
+
+                var msg = "Chip selecionado: ";
+                var chipGroup = viewOfLayout.chip_group;
+                var count: Int = chipGroup.childCount;
+                if (count == 0) {
+                    msg += " nenhum selecionado";
+                } else {
+                    var i: Int = 0;
+                    while (i < count) {
+                        var chip: Chip = chipGroup.getChildAt(i) as Chip;
+                        if (chip.isChecked == true) {
+                            msg += chip.text.toString();
+                        }
+                        i++;
+                    }
+
                 }
+
+                Toast.makeText(activity, msg, Toast.LENGTH_LONG).show();
 
             }
 
-            Toast.makeText(activity,msg, Toast.LENGTH_LONG).show();
 
+            // Inflate the layout for this fragment
+            return viewOfLayout;
         }
 
-        viewOfLayout.chip_group.addView(chip2);
+        fun newInstance(): AddCardapioFragment {
+            return AddCardapioFragment();
+        }
 
-        // Inflate the layout for this fragment
-        return viewOfLayout;
-    }
 
-    fun newInstance() : AddCardapioFragment{
-        return AddCardapioFragment();
-    }
 
 }
