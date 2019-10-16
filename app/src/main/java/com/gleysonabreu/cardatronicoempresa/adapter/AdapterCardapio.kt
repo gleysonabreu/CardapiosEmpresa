7bc27bf862b69a70ebe1d86ebce155dfc8a062bd
Gleyson Abreu <gleyson_datu@hotmail.com>
2019-10-16 12:12:28 -0300

    Sistema de mostrar itens do cardápio realizados, sistema de deletar itens do cardápio com AlertDialog realizados, faltando o sistema de editar itens e ajustar o design da AlertDialog.

---
+++ app/src/main/java/com/gleysonabreu/cardatronicoempresa/adapter/AdapterCardapio.kt
@@ -0,0 +1,86 @@
+package com.gleysonabreu.cardatronicoempresa.adapter
+
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.net.Uri
+import android.provider.Settings
+import android.view.ContextThemeWrapper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.gleysonabreu.cardatronicoempresa.R
+import com.gleysonabreu.cardatronicoempresa.helper.SettingsFirebase
+import com.gleysonabreu.cardatronicoempresa.model.Cardapio
+import com.google.firebase.database.DatabaseReference
+import kotlinx.android.synthetic.main.itens_cardapio.view.*
+
+class AdapterCardapio (private val cardapio: ArrayList<Cardapio>, private val context: Context) : RecyclerView.Adapter<AdapterCardapio.CardapioViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardapioViewHolder {
+
+        val view: View = LayoutInflater.from(context).inflate(R.layout.itens_cardapio, parent, false);
+        return CardapioViewHolder(view);
+
+    }
+
+    override fun getItemCount(): Int {
+        return cardapio.size;
+    }
+
+    override fun onBindViewHolder(holder: CardapioViewHolder, position: Int) {
+        holder.bindItems(cardapio[position]);
+    }
+
+
+    class CardapioViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val nomeProduto = itemView.textTitle;
+        val descricaoProduto = itemView.textDescription;
+        val fotoProduto = itemView.imageProduct;
+        val precoProduto = itemView.textPrice;
+
+        var builder: AlertDialog.Builder = AlertDialog.Builder(ContextThemeWrapper(itemView.context, R.style.AlertDialogCustom));
+
+
+        fun bindItems(cardapio: Cardapio){
+
+            nomeProduto.text = cardapio.nameitem;
+            descricaoProduto.text = cardapio.descriptionItem;
+            precoProduto.text = "R$ " + cardapio.priceitem;
+
+
+            if ( cardapio.photoItem != null ){
+                var url: Uri = Uri.parse(cardapio.photoItem);
+                Glide.with(itemView.context).load(url).into(fotoProduto);
+            }
+
+            itemView.imageButtonEdit.setOnClickListener {
+                Toast.makeText(itemView.context, "Você está querendo editar: "+nomeProduto.text.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            itemView.imageButtonDelete.setOnClickListener {
+
+                builder.setTitle("Excluindo item do cardápio..");
+                builder.setMessage("Você tem certeza que deseja excluir o "+nomeProduto.text.toString()+"?");
+                builder.setPositiveButton("Sim", DialogInterface.OnClickListener(){ dialogInterface: DialogInterface, i: Int ->
+                    deleteCardapio(cardapio.idItemCardapio)
+                });
+                builder.setNegativeButton("Não", null);
+                builder.show();
+            }
+
+        }
+
+        private fun deleteCardapio(idCardapio: String){
+            val firebaseref: DatabaseReference = SettingsFirebase.getFirebase();
+            var cardapioRef = firebaseref.child("cardapio").child(idCardapio);
+            cardapioRef.setValue(null);
+
+            Toast.makeText(itemView.context, nomeProduto.text.toString()+" foi excluído com sucesso!", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+}
--- app/src/main/java/com/gleysonabreu/cardatronicoempresa/fragment/HomeFragment.kt
+++ app/src/main/java/com/gleysonabreu/cardatronicoempresa/fragment/HomeFragment.kt
@@ -8,24 +8,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.gleysonabreu.cardatronicoempresa.R
+import com.gleysonabreu.cardatronicoempresa.adapter.AdapterCardapio
+import com.gleysonabreu.cardatronicoempresa.helper.SettingsFirebase
+import com.gleysonabreu.cardatronicoempresa.model.Cardapio
 import com.google.android.material.chip.Chip
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.fragment_add_cardapio.*
 import kotlinx.android.synthetic.main.fragment_add_cardapio.view.*
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
+import kotlinx.android.synthetic.main.fragment_home.view.recyclerMenuItems
 
 /**
  * A simple [Fragment] subclass.
  */
 class HomeFragment : Fragment() {
 
+    private var listMenu: ArrayList<Cardapio> = ArrayList<Cardapio>();
+    private lateinit var firebaseRef: DatabaseReference;
+    private lateinit var childEvent: ChildEventListener;
+    private lateinit var cardapioRef: DatabaseReference;
+    private lateinit var  linearLayoutManager: LinearLayoutManager;
+    private lateinit var  viewOfLayout: View;
+    private var keyList: ArrayList<String> = ArrayList<String>();
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        var viewOfLayout = inflater!!.inflate(R.layout.fragment_home, container, false);
+        viewOfLayout = inflater!!.inflate(R.layout.fragment_home, container, false);
+
+        linearLayoutManager = LinearLayoutManager(activity);
+
+        //Settings Firebase
+        firebaseRef = SettingsFirebase.getFirebase();
+        cardapioRef = firebaseRef.child("cardapio");
+
+        // Settings Adapter
+        viewOfLayout.recyclerMenuItems.adapter = activity?.let { AdapterCardapio(listMenu, it) }
+        viewOfLayout.recyclerMenuItems.layoutManager = linearLayoutManager;
+
 
         // Inflate the layout for this fragment
         return viewOfLayout;
@@ -33,6 +59,54 @@ class HomeFragment : Fragment() {
 
     fun newInstance() : HomeFragment{
         return HomeFragment();
+    }
+
+    override fun onStart() {
+        super.onStart()
+        listMenu.clear();
+        keyList.clear();
+
+        childEvent = cardapioRef.addChildEventListener(object: ChildEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onChildMoved(p0: DataSnapshot, p1: String?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onChildChanged(p0: DataSnapshot, p1: String?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onChildAdded(p0: DataSnapshot, p1: String?) {
+
+                var newItemmenu = p0.getValue(Cardapio::class.java);
+                if (newItemmenu != null) {
+                    listMenu.add(newItemmenu)
+                    p0.key?.let { keyList.add(it) };
+                };
+
+                viewOfLayout.recyclerMenuItems.adapter?.notifyDataSetChanged();
+
+
+            }
+
+            override fun onChildRemoved(p0: DataSnapshot) {
+                var index: Int = keyList.indexOf(p0.key);
+                listMenu.removeAt(index);
+                keyList.removeAt(index);
+                viewOfLayout.recyclerMenuItems.adapter?.notifyDataSetChanged();
+
+            }
+
+        });
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        cardapioRef.removeEventListener(childEvent);
     }
 
 }
